15427850002b37e6042fedb42b960f0b127a51a5
v1centebarros <vmabarros@ua.pt>
2023-04-21 23:58:40 +0100

    Setup RoadrunnerBackend

---
+++ src/main/java/pt/ua/deti/tqs/roadrunnerbackend/RoadrunnerBackendApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.tqs.roadrunnerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadrunnerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadrunnerBackendApplication.class, args);
+    }
+
+}
