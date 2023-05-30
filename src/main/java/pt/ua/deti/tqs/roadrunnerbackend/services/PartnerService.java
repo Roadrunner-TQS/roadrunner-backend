7541d4fcf9c2fa776671a5dd7ed99eeb5265a04b
mariana <marianaandrade@ua.pt>
2023-05-31 00:55:04 +0100

    implemented the partnerservice

--- src/main/java/pt/ua/deti/tqs/roadrunnerbackend/services/PartnerService.java
+++ src/main/java/pt/ua/deti/tqs/roadrunnerbackend/services/PartnerService.java
@@ -22,10 +22,43 @@ public class PartnerService {
 
 
     public Object getAllPackages(UUID partnerId, String state) {
-       return null;
+        log.info("PartnerService -- getAllPackages -- request");
+        if (!Status.contains(state) && state != null) {
+            log.error("PartnerService -- getAllPackages -- State not valid");
+            return new ErrorDTO("State not valid");
+        }
+
+        if (!pickUpLocationRepository.existsById(partnerId)) {
+            log.error("PartnerService -- getAllPackages -- Partner not found");
+            return new ErrorDTO("Partner not found");
+        }
+        if (state == null) {
+            log.info("PartnerService -- getAllPackages -- Success (without state)");
+            return packageRepository.findAllByPickUpLocationId(partnerId);
+        }
+
+        log.info("Partner Service -- getAllPackages -- Success (with state)");
+        return packageRepository.findAllByPickUpLocationIdAndStatus(partnerId, Status.valueOf(state));
     }
 
     public Object getPackageById(UUID pickUpLocationId, UUID packageId) {
-        return null;
+        log.info("Partner Service -- Getting package by id");
+        Package pack = packageRepository.findById(packageId).orElse(null);
+        if (pack == null) {
+            log.info("Partner Service -- getPackageById -- Package not found");
+            return new ErrorDTO("Package not found");
+        }
+        PickUpLocation pickUpLocation = pickUpLocationRepository.findById(pack.getPickUpLocation().getId()).orElse(null);
+        if (pickUpLocation == null) {
+            log.info("Partner Service -- getPackageById -- PickUpLocation not found");
+            return new ErrorDTO("PickUpLocation not found");
+        }
+
+        if (!pack.getPickUpLocation().getId().equals(pickUpLocationId)) {
+            log.info("Partner Service -- getPackageById -- Package not Authorized");
+            return new ErrorDTO("Package not Authorized");
+        }
+        log.info("Partner Service -- Package found");
+        return pack;
     }
 }
