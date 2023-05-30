9c217c65d53474b7e44d44b384f1a93dafae5ad3
mariana <marianaandrade@ua.pt>
2023-05-31 00:46:55 +0100

    partner service to return the packages the pickuppoint, if the packages is the pickuppoint

---
+++ src/main/java/pt/ua/deti/tqs/roadrunnerbackend/services/PartnerService.java
@@ -0,0 +1,31 @@
+package pt.ua.deti.tqs.roadrunnerbackend.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pt.ua.deti.tqs.roadrunnerbackend.data.PackageRepository;
+import pt.ua.deti.tqs.roadrunnerbackend.data.PickUpLocationRepository;
+import pt.ua.deti.tqs.roadrunnerbackend.model.Package;
+import pt.ua.deti.tqs.roadrunnerbackend.model.PickUpLocation;
+import pt.ua.deti.tqs.roadrunnerbackend.model.dto.ErrorDTO;
+import pt.ua.deti.tqs.roadrunnerbackend.model.enums.Status;
+
+import java.util.UUID;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PartnerService {
+    private final PickUpLocationRepository pickUpLocationRepository;
+    private final PackageRepository packageRepository;
+
+
+    public Object getAllPackages(UUID partnerId, String state) {
+        return null;
+    }
+
+    public Object getPackageById(UUID pickUpLocationId, UUID packageId) {
+        return null;
+    }
+}
