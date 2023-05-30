293accd06cd09dc2662710bbec8918b120a4b799
mariana <marianaandrade@ua.pt>
2023-05-30 16:14:48 +0100

    add methods in repository

--- src/main/java/pt/ua/deti/tqs/roadrunnerbackend/data/PickUpLocationRepository.java
+++ src/main/java/pt/ua/deti/tqs/roadrunnerbackend/data/PickUpLocationRepository.java
@@ -4,8 +4,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pt.ua.deti.tqs.roadrunnerbackend.model.PickUpLocation;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PickUpLocationRepository extends JpaRepository<PickUpLocation, UUID> {
+    List<PickUpLocation> findByCityAndDisable(String city, Boolean disable);
+
+    List<PickUpLocation> findByAcceptedAndDisable(Boolean accepted, Boolean disable);
+
+    List<PickUpLocation> findByCityAndAcceptedAndDisable(String city, Boolean accepted, Boolean disable);
+
+    List<PickUpLocation> findByDisable(Boolean disable);
+
+    Optional<PickUpLocation> findByIdAndDisable(UUID pickUpLocationId, Boolean disable);
+    Optional<PickUpLocation> findByAddress(String address);
 }
