e20d1c2892d68dbc3992add0c90844841bcf4352
mariana <marianaandrade@ua.pt>
2023-05-31 00:08:51 +0100

    implemented the endpoints to get, post and delete info the shop

--- src/main/java/pt/ua/deti/tqs/roadrunnerbackend/controllers/AdminController.java
+++ src/main/java/pt/ua/deti/tqs/roadrunnerbackend/controllers/AdminController.java
@@ -26,35 +26,90 @@ public class AdminController {
     @GetMapping("/shop")
     @PreAuthorize("@authService.isAdmin(#token)")
     public ResponseEntity<Object> getShops(@RequestHeader("Authorization") String token) {
-        return null;
+        log.info("Admin Controller -- Get shops -- request received");
+        Object response = adminService.getShops();
+        if (response instanceof ErrorDTO) {
+            log.error("Admin Controller -- Get shops -- " + ((ErrorDTO) response).getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .contentType(APPLICATION_JSON)
+                    .body(response);
+        }
+        log.info("Admin Controller -- Get shops -- Shops found");
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(APPLICATION_JSON)
+                .body(response);
     }
 
     @PostMapping("/shop")
     @PreAuthorize("@authService.isAdmin(#token)")
     public ResponseEntity<Object> addShop(@RequestHeader("Authorization") String token,
                                           @RequestBody Shop shopRequest) {
-        return null;
+        log.info("Admin Controller -- Add shop -- request received");
+        Shop shop = adminService.addShop(shopRequest);
+        if (shop == null) {
+            log.error("Admin Controller -- Add shop -- Shop not added");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .contentType(APPLICATION_JSON)
+                    .body(new ErrorDTO("Error adding shop"));
+        }
+        log.info("Admin Controller -- Add shop -- Shop added");
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(APPLICATION_JSON)
+                .body(shop);
     }
 
     @GetMapping("/shop/{id}")
     @PreAuthorize("@authService.isAdmin(#token)")
     public ResponseEntity<Object> getShop(@RequestHeader("Authorization") String token,
                                           @PathVariable UUID id){
-        return null;
+        log.info("Admin Controller -- Get shop -- request received");
+        Object response = adminService.getShopById(id);
+        if (response instanceof ErrorDTO) {
+            log.error("Admin Controller -- Get shop -- " + ((ErrorDTO) response).getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .contentType(APPLICATION_JSON)
+                    .body(response);
+        }
+        log.info("Admin Controller -- Get shop -- Shop found");
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(APPLICATION_JSON)
+                .body(response);
     }
 
     @DeleteMapping("/shop/{id}")
     @PreAuthorize("@authService.isAdmin(#token)")
     public ResponseEntity<Object> deleteShop(@RequestHeader("Authorization") String token,
                                              @PathVariable UUID id) {
-        return null;
+        log.info("Admin Controller -- Delete shop -- request received");
+        Boolean deleted = adminService.deleteShop(id);
+        if (Boolean.FALSE.equals(deleted)) {
+            log.error("Admin Controller -- Delete shop -- Shop not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .contentType(APPLICATION_JSON)
+                    .body(new ErrorDTO("Shop not found"));
+        }
+        log.info("Admin Controller -- Delete shop -- Shop removed");
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(APPLICATION_JSON)
+                .body(new SuccessDTO<String>("Shop removed"));
     }
 
     @GetMapping("/shop/package")
     @PreAuthorize("@authService.isAdmin(#token)")
     public ResponseEntity<Object> getPackagesByShop(@RequestHeader("Authorization") String token,
                                                     @RequestParam(value="id") UUID id){
-        return null;
+        log.info("Admin Controller -- Get packages by shop -- request received");
+        Object response = adminService.getPackagesByShop(id);
+        if (response instanceof ErrorDTO) {
+            log.error("Admin Controller -- Get packages by shop -- " + ((ErrorDTO) response).getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .contentType(APPLICATION_JSON)
+                    .body(response);
+        }
+        log.info("Admin Controller -- Get packages by shop -- Packages found");
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(APPLICATION_JSON)
+                .body(response);
     }
 
     @DeleteMapping("/pickup/{id}")
