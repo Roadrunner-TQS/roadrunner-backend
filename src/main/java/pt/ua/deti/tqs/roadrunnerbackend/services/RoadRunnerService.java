e4989f97a4b17bf75eef7eb2bb1738df6aaeb92d
mariana <marianaandrade@ua.pt>
2023-05-31 01:00:21 +0100

    implemented the roadrunnerservice

--- src/main/java/pt/ua/deti/tqs/roadrunnerbackend/services/RoadRunnerService.java
+++ src/main/java/pt/ua/deti/tqs/roadrunnerbackend/services/RoadRunnerService.java
@@ -17,15 +17,72 @@ public class RoadRunnerService {
 
     private static final ErrorDTO UNAUTHORIZED = new ErrorDTO("Unauthorized");
     public Object getPickUpLocationById(UUID pickUpid, String token) {
-        return null;
+        log.info("RoadRunnerService -- Get pick up location by id -- request received");
+        if(Boolean.TRUE.equals(authService.isAdmin(token)) ) {
+            Object response = adminService.getPickUpLocationById(pickUpid);
+            if (response instanceof ErrorDTO) {
+                log.error("RoadRunnerService -- Get pick up location by id -- " + ((ErrorDTO) response).getMessage());
+                return response;
+            }
+            log.info("RoadRunnerService -- Get pick up location by id -- Pick Up Location found");
+            return response;
+        }
+        if (Boolean.TRUE.equals(authService.isPartner(token)) && authService.getCurrentUser(token).getPickUpLocation().getId().equals(pickUpid)) {
+            log.info("RoadRunnerService -- Get pick up location by id -- Pick Up Location found");
+            return authService.getCurrentUser(token).getPickUpLocation();
+        }
+            log.error("RoadRunnerService -- Get pick up location by id -- Unauthorized");
+        return UNAUTHORIZED;
     }
 
     public Object getPackageById(UUID packageId, String token) {
-        return null;
+        log.info("RoadRunnerService -- Get package by id -- request received");
+        if(Boolean.TRUE.equals(authService.isAdmin(token)) ) {
+            Object response = adminService.getPackageById(packageId);
+            if (response instanceof ErrorDTO) {
+                log.error("RoadRunnerService -- Get package by id -- " + ((ErrorDTO) response).getMessage());
+                return response;
+            }
+            log.info("RoadRunnerService -- Get package by id -- Package found");
+            return response;
+        }
+        if (Boolean.TRUE.equals(authService.isPartner(token)))
+        {
+            Object response = partnerService.getPackageById(authService.getCurrentUser(token).getPickUpLocation().getId(), packageId);
+            if (response instanceof ErrorDTO) {
+                log.error("RoadRunnerService -- Get package by id -- " + ((ErrorDTO) response).getMessage());
+                return response;
+            }
+            log.info("RoadRunnerService -- Get package by id -- Package found");
+            return response;
+        }
+        log.error("RoadRunnerService -- Get package by id -- Unauthorized");
+        return UNAUTHORIZED;
     }
 
     public Object getAllPackages(String state, String token) {
-       return null;
+        log.info("RoadRunnerService -- Get all packages -- request received");
+        if(Boolean.TRUE.equals(authService.isAdmin(token)) ) {
+            Object response = adminService.getPackages(state);
+            if (response instanceof ErrorDTO) {
+                log.error("RoadRunnerService -- Get all packages -- " + ((ErrorDTO) response).getMessage());
+                return response;
+            }
+            log.info("RoadRunnerService -- Get all packages -- Packages found");
+            return response;
+        }
+        if (Boolean.TRUE.equals(authService.isPartner(token)))
+        {
+            Object response = partnerService.getAllPackages(authService.getCurrentUser(token).getPickUpLocation().getId(), state);
+            if (response instanceof ErrorDTO) {
+                log.error("RoadRunnerService -- Get all packages -- " + ((ErrorDTO) response).getMessage());
+                return response;
+            }
+            log.info("RoadRunnerService -- Get all packages -- Packages found");
+            return response;
+        }
+        log.error("RoadRunnerService -- Get all packages -- Unauthorized");
+        return UNAUTHORIZED;
     }
 
 }
